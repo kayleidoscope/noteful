85ecb440f62578da8da28112a58fd6f818fa9a0c
Kay Holten <holtenkay@gmail.com>
2020-12-22 12:50:59 -0800

    adds number of notes in folder

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -10,6 +10,7 @@ class Sidebar extends Component {
   static contextType = Context;
 
   render() {
+
     if (this.props.canGoBack) {
       return (
         <aside className="page-sidebar">
@@ -17,13 +18,26 @@ class Sidebar extends Component {
         </aside>
       );
     } else if (this.props.folderSidebar) {
+      const allNotes = this.context.notesStore;
+
+      const notesInFolder = allNotes.filter(note => 
+        note.folder === this.context.folder
+      )
+
+      const numOfNotes = notesInFolder.length
+
       const folderName = this.props.folderName;
       return (
         <aside className="page-sidebar">
           <FolderList/>
           <h3 className="sidebar-folder-name">
             Folder selected: <br/>
-            {folderName}</h3>
+            {folderName}
+          </h3>
+          <p  className="sidebar-folder-name">
+            Number of Notes: <br />
+            {numOfNotes}
+          </p>
         </aside>
       );
     } else {
